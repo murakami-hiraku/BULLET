c6d0c1027316224b4674eda712e1b732d62dc77a
murakami-hiraku <murakami.hirakuwork@gmail.com>
2026-05-07 15:06:04 +0900

    feat: リロード時にクラス付与機能実装

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 // src/main.js
 import "@/scss/style.scss"; // 自分のメインSCSSのパス
+
+// DOMContentLoaded は HTML の解析が終わった瞬間に実行されます
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JSが動いています！"); // デバッグ用：これが出るか確認
+  document.body.classList.add("is-loaded");
+});
